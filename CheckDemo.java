61a7d91371ad85c01f06ac23e44b1f740ae85ec6
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 18:38:59 +0530

    Update CheckDemo.java

--- CheckDemo.java
+++ CheckDemo.java
@@ -1,27 +1,45 @@
-//Use various access modifiers 
-class Check {
-	private int a=5; //declare variables with various access modifiers
-	public int b=78;
-	protected int c=23;
-	int d=12;
-	
-	void disp() { //display private instance member
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
-	}
-	
-}
+
 public class CheckDemo {
 
 	public static void main(String[] args) {
 		
 		Check check=new Check(); //Create object of class Check
-		
-		
-		check.disp();
+		try {
+			check.disp1();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		check.disp2();
+		check.disp3();
+		check.disp4();
 
 	}
 
+}
+
+class Check {
+	//declare variables with various access modifiers
+	private int a=5; 
+	public int b=78;
+	protected int c=23;
+	int d=12;
+	
+	 // Method display
+	private void disp1() throws Exception {		//Gives CompileTime Error : No visibility
+		System.out.println(a);
+	}
+	
+	protected void disp2() {
+		System.out.println(b);
+	}
+	
+	public void disp3() {
+		System.out.println(c);
+	}
+	
+	void disp4() {
+		System.out.println(d);
+	}
+	
 }
