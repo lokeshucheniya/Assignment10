74ed6d00be13879b7b493eba4f2ed6ebcb794893
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 19:21:59 +0530

    Update StaticNonStatic.java

--- StaticNonStatic.java
+++ StaticNonStatic.java
@@ -3,18 +3,18 @@ class TestClass {
 	int no = 5;
 	static String Name="Sample1";
 	
-	int getNo() { //non-static member function
+	int getNo() { 								//non-static member function
 		this.no++;
 		return no;
 	}
-	static String getName() { //static member function
+	static String getName() { 						//static member function
 		return Name;
 	}
 }
 class StaticNonstatic {
 	public static void main(String[] args) {
 		
-		TestClass test = new TestClass(); //Create an object of class TestClass
+		TestClass test = new TestClass(); 				//Create an object of class TestClass
 		
 		//Call static and non-static member functions of class TestClass
 		System.out.println("Next no is "+test.getNo());
