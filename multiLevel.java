b2ece4dc249e15a3a6a2a9d6a8c7927e610ef31c
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 19:35:26 +0530

    Update multiLevel.java

--- multiLevel.java
+++ multiLevel.java
@@ -1,12 +1,11 @@
-
+//Parent Class A
 public class A {
 	void A(){
 		System.out.println("This is main");
 	}
 }
 
-
-
+//Class B inheriting A
 public class B extends A {
 	void B(){
 		A b = new B();
@@ -15,11 +14,21 @@ public class B extends A {
 	}
 }
 
+//Class C inheriting B
+public class C extends B{
+	void C(){
+		B c = new C();
+		c.B();
+		System.out.println("This is child class");
+	}
+}
+
 
 
 public class multiLevel  {
 	public static void main(String args[]) {
-		B b = new B();
-		b.B();
+		C c = new C();
+		c.C();
 	}
 }
+
